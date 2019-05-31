cbe4bd55df701c7befc2552657d6758b06b824a9
Jonathan Dunne <jonathan@flexdapps.com>
2019-05-31 14:38:24 +1000

    blah

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,8 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance });
+
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -39,7 +40,7 @@ class App extends Component {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    await contract.methods.set('liam').send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
     const response = await contract.methods.get().call();
@@ -48,22 +49,25 @@ class App extends Component {
     this.setState({ storageValue: response });
   };
 
+  sleep = async () => {
+    const { accounts, contract } = this.state;
+    await contract.methods.set('liam').send({ from: accounts[0] });
+    const response = await contract.methods.get().call();
+    this.setState({ storageValue: response });
+  }
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
+        <div className>
+
+        </div>
+        <button onClick = {() => this.sleep()}> 
+          nap nap nap
+        </button>
         <div>The stored value is: {this.state.storageValue}</div>
       </div>
     );
