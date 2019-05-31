b4c64b92839585a984e85af2fa40eea639fd52c9
Liam Aharon <liam.aharon@hotmail.com>
2019-05-31 14:27:37 +1000

    contracts

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,13 +1,20 @@
 pragma solidity ^0.5.0;
 
 contract SimpleStorage {
-  uint storedData;
+  uint expireBlock;
+  string name;
 
-  function set(uint x) public {
-    storedData = x;
+  function set(string memory nameParam) public {
+    require(block.number >= expireBlock, 'someone is napping');
+    name = nameParam;
+    expireBlock = block.number + 80;
   }
 
-  function get() public view returns (uint) {
-    return storedData;
+  function getName() public view returns (string memory) {
+    return name;
+  }
+
+  function getExpireBlock() public view returns (uint) {
+    return expireBlock;
   }
 }
